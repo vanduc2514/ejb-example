70c732fa4433c16159d69f9c111798506220b2a0
Nguyen Van Duc <duc03.nguyenvan@ifisolution.com>
2020-10-19 18:09:22 +0700

    add client

--- ejb-client/src/main/java/com/ifi/App.java
+++ ejb-client/src/main/java/com/ifi/App.java
@@ -1,13 +1,18 @@
 package com.ifi;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.ifi.api.EmployeeService;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    EmployeeService employeeService() {
+        return null;
     }
 }
